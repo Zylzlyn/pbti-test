74c1625d97f959ec8af023df1b0b6495b1e76040
Zylzlyn <19328780523@163.com>
2026-06-24 23:42:53 +0800

    fix deploy paths

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
+  // 关键：GitHub Pages 子路径
   basePath: "/pbti-test",
   assetPrefix: "/pbti-test/",
+
+  // 静态导出必须
+  trailingSlash: true,
 
   images: {
     unoptimized: true,
   },
+
+  // ❗删除 eslint 配置（你之前已经报错过）
 };
 
 export default nextConfig;
