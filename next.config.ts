2b7c311662f2a43a78ac8265c457bc13aaaaaf73
Zylzlyn <19328780523@163.com>
2026-06-24 18:54:30 +0800

    清理项目：删除未使用的文件和组件

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/pbti-test",
-  images: {
-    unoptimized: true,
+  reactStrictMode: true,
+  turbopack: {
+    root: __dirname,
   },
 };
 
